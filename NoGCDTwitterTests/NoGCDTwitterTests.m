d22fd2e40439d5b555def684fef90df8663a13d2
T.mik <mikami10mysoft@gmail.com>
2013-04-11 17:05:58 +0900

    Initial Commit

---
+++ NoGCDTwitterTests/NoGCDTwitterTests.m
@@ -0,0 +1,32 @@
+//
+//  NoGCDTwitterTests.m
+//  NoGCDTwitterTests
+//
+//  Created by mikanovic on 2013/04/11.
+//  Copyright (c) 2013年 mikanovic20. All rights reserved.
+//
+
+#import "NoGCDTwitterTests.h"
+
+@implementation NoGCDTwitterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NoGCDTwitterTests");
+}
+
+@end
